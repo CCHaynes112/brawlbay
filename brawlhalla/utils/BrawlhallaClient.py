56ebd8a89e47939f1cb31d395d9d0e14135ae246
Curtis Haynes <CCHaynes1122@gmail.com>
2020-09-12 23:08:15 -0700

    Enable api client to pull information from API and create/update user.

--- brawlhalla/utils/BrawlhallaClient.py
+++ brawlhalla/utils/BrawlhallaClient.py
@@ -1,16 +1,81 @@
 from secret_keys import brawl_key
 import requests
+from ..models import BrawlhallaUser
 
 
 class BrawlhallaClient:
-    def update_user_from_api(id):
-        user_data = requests.get(
+    def get_user_data(self, id):
+        return requests.get(
             "https://api.brawlhalla.com/player/"
             + str(id)
             + "/stats?api_key="
             + brawl_key
+        ).json()
+
+    def update_user_from_api(self, id):
+        user_data = self.get_user_data(id)
+
+        return BrawlhallaUser.objects.filter(
+            brawlhalla_id=user_data["brawlhalla_id"]
+        ).update(
+            name=user_data["name"],
+            xp=user_data["xp"],
+            level=user_data["level"],
+            xp_percent=user_data["xp_percentage"],
+            games=user_data["games"],
+            wins=user_data["wins"],
+            damage_bomb=user_data["damagebomb"],
+            damage_mine=user_data["damagemine"],
+            damage_spikeball=user_data["damagespikeball"],
+            damage_sidekick=user_data["damagesidekick"],
+            hit_snowball=user_data["hitsnowball"],
+            ko_bomb=user_data["kobomb"],
+            ko_spike=user_data["kospikeball"],
+            ko_sidekick=user_data["kosidekick"],
+            ko_snowball=user_data["kosnowball"],
         )
-        
-    
-    def create_user_from_api(id):
-        pass
+
+    def update_user_legends_from_api(self, id):
+        user_data = self.get_user_data(id)
+
+        return BrawlhallaUser.objects.filter(
+            brawlhalla_id=user_data["brawlhalla_id"]
+        ).update(
+            name=user_data["name"],
+            xp=user_data["xp"],
+            level=user_data["level"],
+            xp_percent=user_data["xp_percentage"],
+            games=user_data["games"],
+            wins=user_data["wins"],
+            damage_bomb=user_data["damagebomb"],
+            damage_mine=user_data["damagemine"],
+            damage_spikeball=user_data["damagespikeball"],
+            damage_sidekick=user_data["damagesidekick"],
+            hit_snowball=user_data["hitsnowball"],
+            ko_bomb=user_data["kobomb"],
+            ko_spike=user_data["kospikeball"],
+            ko_sidekick=user_data["kosidekick"],
+            ko_snowball=user_data["kosnowball"],
+        )
+
+    def create_user_from_api(self, id):
+        user_data = self.get_user_data(id)
+
+        return BrawlhallaUser.objects.create(
+            brawlhalla_id=user_data["brawlhalla_id"],
+            name=user_data["name"],
+            xp=user_data["xp"],
+            level=user_data["level"],
+            xp_percent=user_data["xp_percentage"],
+            games=user_data["games"],
+            wins=user_data["wins"],
+            damage_bomb=user_data["damagebomb"],
+            damage_mine=user_data["damagemine"],
+            damage_spikeball=user_data["damagespikeball"],
+            damage_sidekick=user_data["damagesidekick"],
+            hit_snowball=user_data["hitsnowball"],
+            ko_bomb=user_data["kobomb"],
+            ko_spike=user_data["kospikeball"],
+            ko_sidekick=user_data["kosidekick"],
+            ko_snowball=user_data["kosnowball"],
+        )
--- brawlhalla/views.py
+++ brawlhalla/views.py
@@ -1,26 +1,31 @@
 from django.views.generic import View
-from django.shortcuts import get_object_or_404
 from .models import BrawlhallaUser
 from .schemas import BrawlhallaUserSchema
 from django.http import JsonResponse
-
-
-# 5 minutes
-API_REFRESH_RATE = 60 * 5
+from .utils.BrawlhallaClient import BrawlhallaClient
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
+import datetime
 
 
 class BrawlhallaUserView(View):
     def get(self, request, brawlhalla_id):
         try:
             user = BrawlhallaUser.objects.get(brawlhalla_id=brawlhalla_id)
-            if user.updated_at < API_REFRESH_RATE:
+            if timezone.now() - user.updated_at < datetime.timedelta(minutes=5):
                 return JsonResponse({"data": BrawlhallaUserSchema().dump(user)})
             else:
                 # Get user from API
                 # Update user
+                client = BrawlhallaClient()
+                updated_user = client.update_user_from_api(brawlhalla_id)
                 # Return updated user
+                return JsonResponse({"data": BrawlhallaUserSchema().dump(updated_user)})
         except ObjectDoesNotExist:
             # Create user from API
+            client = BrawlhallaClient()
+            created_user = client.create_user_from_api(brawlhalla_id)
+            return JsonResponse({"data": BrawlhallaUserSchema().dump(created_user)})
 
 
 class BrawlhallaUsersView(View):
