e07e6ab066f6fe483060e75c743c33186981973a
Curtis Haynes <CCHaynes1122@gmail.com>
2021-01-03 22:56:39 -0800

    Add allowed hosts and set debug False

--- backend/brawlbackend/settings.py
+++ backend/brawlbackend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_keys.django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "brawlbay.com", "www.brawlbay.com"]
 
 
 # Application definition
