9b403d44aafb1b8fe4762f1e13a7b99df4664ff5
Curtis Haynes <CCHaynes1122@gmail.com>
2021-01-05 21:38:15 -0800

    Set up different dbs for dev/prod based on debug mode. This is going to change when I set up a postgres container for dev

--- backend/brawlbackend/settings.py
+++ backend/brawlbackend/settings.py
@@ -78,22 +78,24 @@ WSGI_APPLICATION = "brawlbackend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if DEBUG is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "postgres",
-#         "PORT": 5432,
-#     }
-# }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": secret_keys.db_name,
+            "USER": secret_keys.db_username,
+            "PASSWORD": secret_keys.db_password,
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
+    }
 
 
 # Password validation
