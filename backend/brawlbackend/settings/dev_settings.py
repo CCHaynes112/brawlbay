6f2a70d101664a9f1ff455d9394ccb1a28d9bdb8
Curtis Haynes <CCHaynes1122@gmail.com>
2021-01-09 11:12:38 -0800

    Oops forgot the settings directory

---
+++ backend/brawlbackend/settings/dev_settings.py
@@ -0,0 +1,11 @@
+import os
+from .base_settings import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
---
+++ backend/brawlbackend/settings/prod_settings.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": secret_keys.db_name,
+        "USER": secret_keys.db_username,
+        "PASSWORD": secret_keys.db_password,
+        "HOST": "172.17.0.1",
+        "PORT": 5432,
+    }
+}
