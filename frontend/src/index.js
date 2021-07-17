0b0b613e5ac405410c32bb73f3c21ddf7c27c22f
Curtis Haynes <CCHaynes1122@gmail.com>
2021-07-17 00:31:45 -0700

    Init sentry if env isn't development

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,13 +9,11 @@ import { Integrations } from "@sentry/tracing";
 const trackingId = 'UA-83277161-1'
 ReactGA.initialize(trackingId)
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV != "development") {
     Sentry.init({
         dsn: "https://6b54cef707c9494babd5563c9904df41@o552500.ingest.sentry.io/5869041",
         integrations: [new Integrations.BrowserTracing()],
         tracesSampleRate: 1.0,
     });
 }
-
-
 ReactDOM.render(<App />, document.getElementById('root'))
