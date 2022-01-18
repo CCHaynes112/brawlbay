f950629a70b5f03a718e1e6b64bc3766dd0af447
Curtis <cchaynes1122@gmail.com>
2022-01-18 11:06:00 -0800

    bump superlinter version

--- backend/scripts/resize_images.py
+++ backend/scripts/resize_images.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 
-directory = r'C:\Users\CCHay\Desktop\fixed_images'
+directory = r"C:\Users\CCHay\Desktop\fixed_images"
 for filename in os.listdir(directory):
     print(f"Working on {filename}")
     if filename.endswith(".png"):
